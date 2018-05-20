f7ebf7e4f737e85b56872680fe3337e2c5729e83
Sav Younts <savyounts@gmail.com>
2018-05-20 21:32:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,17 @@ function getIt(){
 }
 
 function pressIt(){
-  
+  $('input').on('keydown', function(key){
+    if (key.which === 17) {
+      alert ("you have pressed G")
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert ("Your for is going to be submitted now.")
+  })
 }
 
 $(document).ready(function(){
